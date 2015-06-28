9b28d00515ca6805dfbd79ad71a992ae39f10d99
cxzzy <wies_kueter@hotmail.com>
2015-06-28 02:28:32 +0200

    Detail page styled

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -14,50 +14,56 @@
 		</div>
 	</div>
 
-
-	<div class="col s12 m12 l12">
-		<div class="card blue-grey darken-1">
-			<div class="card-content">
-				<div class="row">
-					<div class="col s6">
-						<img src="https://avatars3.githubusercontent.com/u/1228272?v=3&amp;s=460" class="circle">
-					</div>
-					<div class="col s6">
-						<span class="card-title grey-text">Wies Kueter</span>
-						<h3>Hoe bouw ik een hut?</h3>
-						<p>Ik wil een hut bouwen maar weet niet zo goed hoe ik moet geginnen..</p>
+	
+	<div class="row">
+		<div class="col s12 m12 l12">
+			<div class="card">
+				<div class="card-content">
+					<div class="row">
+						<div class="col s6">
+							<img src="https://avatars3.githubusercontent.com/u/1228272?v=3&amp;s=460" class="circle">
+						</div>
+						<div class="col s6">
+							<span class="card-title grey-text">Wies Kueter</span>
+							<h3>Hoe bouw ik een hut?</h3>
+							<p>Ik wil een hut bouwen maar weet niet zo goed hoe ik moet geginnen..</p>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
+	
+	<div class="container">
+		<div class="row card solutions">
 			
-		<ul class="collection with-header">
-			@for($i = 0; $i < 10; $i++)
-				<li class="avatar">
-					<div class="collection-header">
-						<div class="row">
-							<div class="col s6">
-								<img src="https://avatars3.githubusercontent.com/u/1228272?v=3&amp;s=460" class="circle">
+			<ul class="collection with-header">
+				@for($i = 0; $i < 10; $i++)
+					<li class="avatar">
+						<div class="collection-header">
+							<div class="row">
+								<div class="col s6">
+									<img src="https://avatars3.githubusercontent.com/u/1228272?v=3&amp;s=460" class="circle">
+								</div>
+								<div class="col s6">
+									<span class="card-title grey-text">Dmitri</span>
+								</div>
+								<p>Dkadkj kja kjd aakjdf klj akj a</p>
 							</div>
-							<div class="col s6">
-								<span class="card-title grey-text">Dmitri</span>
-							</div>
-							<p>Dkadkj kja kjd aakjdf klj akj a</p>
 						</div>
-					</div>
 
-					<div class="solution-documents card-action">
-						<a href="#modal1" data-target="modal1" class="modal-trigger">Video</a> <a href="">Explaination</a>
-					</div>
+						<div class="solution-documents card-action">
+							<a href="#modal1" data-target="modal1" class="modal-trigger">Video</a> <a href="">Explaination</a>
+						</div>
 
-					<div class="">
-						<a class="btn-floating btn-large waves-effect waves-light red"></a>
-						<a class="btn-floating btn-large waves-effect waves-light green"></a>
-					</div>
-				</li>
-			@endfor
-		</ul>
-
+						<div class="">
+							<a class="btn-floating btn-large waves-effect waves-light red"></a>
+							<a class="btn-floating btn-large waves-effect waves-light green"></a>
+						</div>
+					</li>
+				@endfor
+			</ul>
+		</div>
 	</div>
 @stop
 
